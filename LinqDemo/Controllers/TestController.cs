c5398ae66d8b332b15ecefa01a2523e1c53b2062
Aishwarya28 <aishwaryakote99@gmail.com>
2022-03-10 03:10:45 -0800

    files

---
+++ LinqDemo/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using LinqDemo.DemoLinq;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[Action]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult Stud()
+        {
+            TestRepository tr = new TestRepository();
+            List<Student> details = tr.Stud();
+            return Ok(details); 
+        }
+        [HttpGet]
+        public IActionResult GetAllStudent(int Rollno)
+        { 
+        
+            TestRepository tr = new TestRepository();
+            Student details = tr.GetAllStudent(Rollno);
+            return Ok(details);
+        }
+
+    }
+}
---
+++ LinqDemo/DemoLinq/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinqDemo.DemoLinq
+{
+    public class Student
+    {
+        public int Rollno { get; set; }
+        public string name { get; set; }
+        public string division { get; set; }
+
+
+
+    }
+}
